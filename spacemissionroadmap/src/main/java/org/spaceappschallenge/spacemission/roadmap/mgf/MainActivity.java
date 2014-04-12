8121385cbcfb6c061785b53af7bf30118f809ad1
Luis Feliu <lfeliu@redpointtech.com>
2014-04-12 15:12:28 -0500

    GCM boiler plate.

--- spacemissionroadmap/src/main/java/org/spaceappschallenge/spacemission/roadmap/mgf/MainActivity.java
+++ spacemissionroadmap/src/main/java/org/spaceappschallenge/spacemission/roadmap/mgf/MainActivity.java
@@ -4,8 +4,13 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,9 +18,26 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
 
-public class MainActivity extends Activity
-        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+import java.io.IOException;
+
+
+public class MainActivity extends Activity implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final String PREFERENCES_PROPERTY_REG_ID = "registration_id";
+    private static final String PREFERENCES_PROPERTY_APP_VERSION = "appVersion";
+
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+
+    private GoogleCloudMessaging gcm;
+    private String SENDER_ID = "apps-sender-id";//TODO: provide id (project number from API Console)
+    private String regid;
+    private Context context;
 
     /**
      * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
@@ -32,14 +54,28 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mNavigationDrawerFragment = (NavigationDrawerFragment)
-                getFragmentManager().findFragmentById(R.id.navigation_drawer);
+        context = getApplicationContext();
+
+        mNavigationDrawerFragment = (NavigationDrawerFragment) getFragmentManager().findFragmentById(R.id.navigation_drawer);
         mTitle = getTitle();
 
         // Set up the drawer.
-        mNavigationDrawerFragment.setUp(
-                R.id.navigation_drawer,
-                (DrawerLayout) findViewById(R.id.drawer_layout));
+        mNavigationDrawerFragment.setUp(R.id.navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout));
+
+        // Check device for Play Services APK. If check succeeds, proceed with GCM registration.
+        if (checkPlayServices()) {
+
+            gcm = GoogleCloudMessaging.getInstance(this);
+            regid = getRegistrationId(context);
+
+            if (regid.isEmpty()) {
+                registerInBackground();
+            }
+
+        } else {
+            Log.i(TAG, "No valid Google Play Services APK found.");
+        }
+
     }
 
     @Override
@@ -138,6 +174,152 @@ public class MainActivity extends Activity
             ((MainActivity) activity).onSectionAttached(
                     getArguments().getInt(ARG_SECTION_NUMBER));
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Check device for Play Services APK.
+        checkPlayServices();
+    }
+
+    /**
+     * Check the device to make sure it has the Google Play Services APK. If
+     * it doesn't, display a dialog that allows users to download the APK from
+     * the Google Play Store or enable it in the device's system settings.
+     */
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this, PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Log.i(TAG, "This device is not supported.");//TODO: Show dialog to user
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Stores the registration ID and the app versionCode in the application's
+     * {@code SharedPreferences}.
+     *
+     * @param context application's context.
+     * @param regId registration ID
+     */
+    private void storeRegistrationId(Context context, String regId) {
+        final SharedPreferences prefs = getGcmPreferences();
+        int appVersion = getAppVersion(context);
+        Log.i(TAG, "Saving regId on app version " + appVersion);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(PREFERENCES_PROPERTY_REG_ID, regId);
+        editor.putInt(PREFERENCES_PROPERTY_APP_VERSION, appVersion);
+        editor.commit();
+    }
+
+    /**
+     * Gets the current registration ID for application on GCM service, if there is one.
+     * <p>
+     * If result is empty, the app needs to register.
+     *
+     * @return registration ID, or empty string if there is no existing
+     *         registration ID.
+     */
+    private String getRegistrationId(final Context context) {
+
+        final SharedPreferences prefs = getGcmPreferences();
+
+        final String registrationId = prefs.getString(PREFERENCES_PROPERTY_REG_ID, "");
+
+        if (registrationId.isEmpty()) {
+            Log.i(TAG, "Registration not found.");
+            return "";
+        }
+
+        // Check if app was updated; if so, it must clear the registration ID
+        // since the existing regID is not guaranteed to work with the new
+        // app version.
+
+        final int registeredVersion = prefs.getInt(PREFERENCES_PROPERTY_APP_VERSION, Integer.MIN_VALUE);
+        final int currentVersion = getAppVersion(context);
+
+        if (registeredVersion != currentVersion) {
+            Log.i(TAG, "App version changed.");
+            return "";
+        }
+
+        return registrationId;
+
+    }
+
+    /**
+     * Registers the application with GCM servers asynchronously.
+     * <p>
+     * Stores the registration ID and the app versionCode in the application's
+     * shared preferences.
+     */
+    private void registerInBackground() {
+
+        final Runnable registrationRunnable = new Runnable() {
+            @Override
+            public void run() {
+                try {
+
+                    if (gcm == null) {
+                        gcm = GoogleCloudMessaging.getInstance(context);
+                    }
+
+                    regid = gcm.register(SENDER_ID);
+
+                    Log.i(TAG, "Device registered, registration ID=" + regid);
+
+                    // Send the registration ID to server
+                    sendRegistrationIdToBackend();
+
+                    // Persist the regID - no need to register again.
+                    storeRegistrationId(context, regid);
+
+                } catch (IOException ex) {
+                   Log.e(TAG, "Error :" + ex.getMessage());
+                    //TODO: If there is an error,require user action.
+                }
+            }
+        };
+
+        final Thread registrationThread = new Thread(registrationRunnable);
+        registrationThread.setName("Space Apps Challenge GCM Registration Thread");
+        registrationThread.start();
+
+    }
+
+    /**
+     * @return Application's version code from the {@code PackageManager}.
+     */
+    private static int getAppVersion(final Context context) {
+        try {
+            PackageInfo packageInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+            return packageInfo.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            // should never happen
+            throw new RuntimeException("Could not get package name: " + e);
+        }
+    }
+
+    /**
+     * @return Application's {@code SharedPreferences}.
+     */
+    private SharedPreferences getGcmPreferences() {
+        return getSharedPreferences(TAG, Context.MODE_PRIVATE);
+    }
+
+    /**
+     * Sends the registration ID to server over HTTP, so it can use GCM/HTTP or CCS to send
+     * messages to the app.
+     */
+    private void sendRegistrationIdToBackend() {
+        //TODO: send registration id to server.
     }
 
 }
